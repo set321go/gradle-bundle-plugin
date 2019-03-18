a97980476d603df7af0cc1d75e7f0d199c2e39f4
Alex Edwards <alex.edwards@data-realm.com>
2019-03-18 13:20:05 -0700

    Update to the last version of gradle 4, fixed compile issues and build deprecation warnings, added new version to itest versions.

--- src/test/groovy/org/dm/gradle/plugins/bundle/ManifestSubstituteSpec.groovy
+++ src/test/groovy/org/dm/gradle/plugins/bundle/ManifestSubstituteSpec.groovy
@@ -2,6 +2,7 @@ package org.dm.gradle.plugins.bundle
 
 import org.codehaus.groovy.control.io.NullWriter
 import org.gradle.api.java.archives.Manifest
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class ManifestSubstituteSpec extends Specification {
@@ -17,6 +18,7 @@ class ManifestSubstituteSpec extends Specification {
         jarBuilder = Mock(JarBuilder)
     }
 
+    @Ignore("This method was removed in 4.x so this test should no longer be needed")
     def "Redirects writeTo for Writer"() {
         when:
         substitute.writeTo(NullWriter.DEFAULT)
